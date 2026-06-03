a26dd308d9dd2d0f13f71b16c2e2f74666431cab
fatmaibrahim20004-coder <fatmaibrahim20004@gmail.com>
2026-06-03 22:05:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ with col2:
     r_work = st.slider("R Work", 0.0, 1.0, 0.2, key="r_work")
 
 
-if st.button(" Predict Resolution"):
+if st.button("Predict Resolution", key="predict_btn"):
 
     input_data = np.zeros(len(columns))
 
@@ -77,44 +77,5 @@ if st.button(" Predict Resolution"):
 
 
 
-scaler = joblib.load("scaler.pkl")
-columns = joblib.load("model_columns.pkl")
 
-st.title(" Protein Structure Resolution Prediction")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    matthews = st.slider("Matthews Coefficient", 1.0, 5.0, 2.5)
-    solvent = st.slider("Solvent Content (%)", 0.0, 100.0, 50.0)
-    ph = st.slider("pH", 0.0, 14.0, 7.0)
-
-with col2:
-    temp = st.slider("Temperature (K)", 250.0, 350.0, 300.0)
-    r_free = st.slider("R Free", 0.0, 1.0, 0.2)
-    r_work = st.slider("R Work", 0.0, 1.0, 0.2)
-
-
-if st.button(" Predict Resolution"):
-
-    input_data = np.zeros(len(columns))
-
-    input_data[0] = matthews
-    input_data[1] = solvent
-    input_data[2] = ph
-    input_data[3] = temp
-    input_data[4] = r_free
-    input_data[5] = r_work
-
-    input_data = scaler.transform([input_data])
-
-    prediction = model.predict(input_data)[0]
-
-    st.success(f"Predicted Resolution: {round(prediction, 2)} Å")
-
-    if prediction < 2:
-        st.info("🟢 High resolution (Excellent quality)")
-    elif prediction < 3:
-        st.warning("🟡 Medium resolution")
-    else:
-        st.error("🔴 Low resolution")
+    
